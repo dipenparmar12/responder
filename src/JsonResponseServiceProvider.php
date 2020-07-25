11c189a8b6e841f1fb026f160548373252767fd2
dipen parmar <dipensavji@gmail.com>
2020-07-25 12:49:22 +0530

    Json format update

--- src/JsonResponseServiceProvider.php
+++ src/JsonResponseServiceProvider.php
@@ -3,54 +3,69 @@
 namespace Dipenparmar12\Responder;
 
 use Illuminate\Contracts\Routing\ResponseFactory;
+use Illuminate\Support\Collection;
+use Illuminate\Support\MessageBag;
 use Illuminate\Support\ServiceProvider;
-
-/// To registor 
-/// /config/app.php
-/// App\Providers\JsonResponseServiceProvider::class,
-//  'providers' => [
-//        // ...
-//        App\Providers\JsonResponseServiceProvider::class,
-//    ],
-//    How to use it.
-//    return response()->success("res msg",[1,2,],null,401);
-//    return response()->error("error name");
 
 class JsonResponseServiceProvider extends ServiceProvider
 {
+
     /**
      * Bootstrap services.
+     *
+     * @param ResponseFactory $factory
      *
      * @return void
      */
     public function boot(ResponseFactory $factory)
     {
-        $factory->macro('success', function ($message, $data = [], $subStatus = NULL, $finalStatus = 200) use ($factory) {
-            return $factory->make([
-                'success' => true,
-                'message' => $message,
+        $factory->macro('success', function (string $message, $data = [], $subStatus = NULL, $finalStatus = 200, array $rules = []) use ($factory) {
+
+            $response_payload = ['success' => true,
                 'status' => $subStatus ?? 200,
-                'path' => '/' . request()->path(),
-                'results' => $data,
-                'metadata' => [
-                    'auth_id' => auth()->id() ?? null,
-                    'url' => request()->url()
-                ],
-            ], $finalStatus);
+                'message' => is_string($message) ? $message : (string)$message,
+                'requestLocation' => request()->path(),
+                'rules' => [],
+            ];
+
+            if (isset($rules) and $rules) {
+                $response_payload['rules'] = $rules;
+            }
+
+            if (isset($data) and $data instanceof Collection) {
+                $response_payload['results'] = collect($data)->toArray();
+            } else {
+                $response_payload['results'] = is_array($data) ? $data : [$data];
+            }
+
+            $response_payload['metadata'] = ['auth_id' => auth()->id() ?? null, 'url' => request()->url()];
+            return $factory->make($response_payload, $finalStatus);
         });
 
-        $factory->macro('error', function ($message, $data = [], $subStatus = NULL, $finalStatus = 500) use ($factory) {
+        $factory->macro('error', function ($message, $data = [], $subStatus = NULL, $finalStatus = 200, array $rules = []) use ($factory) {
             $response_payload = [
                 'success' => false,
-                'message' => $message,
                 'status' => $subStatus ?? 400,
-                'path' => '/' . request()->path(),
-                'results' => $data,
-                'metadata' => [
-                    'auth_id' => auth()->id() ?? null,
-                    'url' => request()->url()
-                ],
+                'requestLocation' => request()->path(),
+                'message' => is_string($message) ? $message : (string)$message,
             ];
+
+            if (isset($rules) and $rules) {
+                $response_payload['rules'] = $rules;
+            }
+
+            /*if (isset($data['rules'])) {
+                $response_payload['rules'] = $data['rules'];
+                unset($data['rules']);
+            }*/
+
+            if (isset($data) and $data instanceof MessageBag) {
+                $response_payload['results'] = [collect($data)->toArray()];
+            } else {
+                $response_payload['results'] = is_array($data) ? $data : [$data];
+            }
+
+            $response_payload['metadata'] = ['auth_id' => auth()->id() ?? null, 'url' => request()->url()];
             return $factory->make($response_payload, $finalStatus);
         });
     }
@@ -64,3 +79,14 @@ class JsonResponseServiceProvider extends ServiceProvider
     {
     }
 }
+
+/// To registor
+/// /config/app.php
+/// App\Providers\JsonResponseServiceProvider::class,
+//  'providers' => [
+//        // ...
+//        App\Providers\JsonResponseServiceProvider::class,
+//    ],
+//    How to use it.
+//    return response()->success("res msg",[1,2,],null,401);
+//    return response()->error("error name");
